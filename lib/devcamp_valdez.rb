07fcb7bbd07e62d78ef8fa9a13e76cffa4f4ca26
vald3z <vald3z.cat@gmail.com>
2017-07-06 16:45:18 +0000

    dev tool initial commit

---
+++ lib/devcamp_valdez.rb
@@ -0,0 +1,6 @@
+require "devcamp_valdez/version"
+require "devcamp_valdez/renderer"
+
+module DevcampValdez
+  # Your code goes here...
+end
---
+++ lib/devcamp_valdez/version.rb
@@ -0,0 +1,3 @@
+module DevcampValdez
+  VERSION = "0.1.0"
+end
